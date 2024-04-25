628d7046bf210fc9127cbfa71fa49a25d9667bf1
Mavrik-jnr <mahmoud4u12@gmail.com>
2024-04-24 20:46:45 -0400

    design: Add steps to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-28 gap-64 ">
+    <main className="flex min-h-screen flex-col items-center justify-center py-28 gap-64 w-full ">
       <section className="flex flex-row flex-auto gap-10 items-center w-full justify-between pt-20 max-w-[1440px] ">
         <div className="flex items-start flex-col flex-initial gap-6 w-1/2">
           <h1 className="text-6xl font-bold">
@@ -167,13 +167,141 @@ export default function Home() {
           </Link>
         </header>
         <div className=" grid grid-cols-3  gap-14  w-full">
-          {
-            staticNfts.map((item,idx)=>(
-              <NftCard key={idx} nftImg={item.nftImg} title={item.title} artist={item.artist} artistName={item.artistName}/>
-            ))
-          }
+          {staticNfts.map((item, idx) => (
+            <NftCard
+              key={idx}
+              nftImg={item.nftImg}
+              title={item.title}
+              artist={item.artist}
+              artistName={item.artistName}
+            />
+          ))}
         </div>
+      </section>
+      <section className="w-full ">
+        <div className="w-full relative h-[660px] ">
+          <span
+            className="flex justify-center items-end absolute -inset-0 z-10 bg-gradient-to-b from-[#A259FF00] to-cta"
+            aria-hidden="true"
+          >
+            <div className="w-full flex flex-col px-72 h-1/2  gap-8">
+              <div className="flex flex-initial gap-2 bg-secondary w-fit px-5 py-2 rounded-3xl justify-between">
+                <Image
+                  src="/images/Avatars/Avatar4.svg"
+                  width="24"
+                  height="24"
+                  alt="artist"
+                ></Image>
+                <p className="text-base">Shroomie</p>
+              </div>
+              <div className="flex justify-between items-center">
+                <div className="flex flex-col justify-between gap-8">
+                  <h1 className="text-6xl">Magic Mushrooms</h1>
+                  <Link
+                    href="/rankings"
+                    className="flex items-center py-4 px-12 gap-4 m-0 h-fit w-fit rounded-xl  bg-white text-secondary"
+                  >
+                    <Image
+                      src="/images/icons/eye.svg"
+                      width="24"
+                      height="24"
+                      alt="rocket launch"
+                    ></Image>{" "}
+                    <p>See all</p>
+                  </Link>
+                </div>
+                <div>
+                  <article className="bg-[#3b3b3b81] p-8 rounded-3xl">
+                    <p className="text-white text-xs mb-3">Auction ends in:</p>
+                    <div className="flex gap-2">
+                      <div className="flex flex-col gap-3 items-center">
+                        <p className="text-5xl font-bold">59</p>
+                        <p className="text-xs">Hours</p>
+                      </div>
+                      <p className="text-5xl font-bold">:</p>
+                      <div className="flex flex-col gap-3 items-center">
+                        <p className="text-5xl font-bold">59</p>
+                        <p className="text-xs">Minutes</p>
+                      </div>
+                      <p className="text-5xl font-bold">:</p>
+                      <div className="flex flex-col gap-3 items-center">
+                        <p className="text-5xl font-bold">59</p>
+                        <p className="text-xs">Seconds</p>
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </span>
+          <Image
+            src="/images/nfts/mushroom.png"
+            alt="art category"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          ></Image>
+        </div>
+      </section>
+      <section className="w-full max-w-[1440px] ">
+        <header className="mb-8">
+          <h1 className="text-4xl font-bold">How it Works</h1>
+          <p className="mt-4 text-2xl">Find out how to get started</p>
+        </header>
+        <div className=" grid grid-cols-3  gap-14  w-full">
+          <article className="bg-secondary p-8 w-full max-w-[330px]  flex flex-col gap-5 items-center rounded-3xl">
+            <Image
+              src="/images/steps/wallet.svg"
+              alt="art category"
+              // layout="fill"
+              width="250"
+              height="250"
+              objectFit="cover"
+            ></Image>
 
+            <div className="text-center">
+              <h3 className="text-2xl  font-bold mb-2">Setup Your Wallet</h3>
+              <p className="text-base">
+                Set up your wallet of choice. Connect it to Vyro by clicking the
+                wallet icon in the top right corner.
+              </p>
+            </div>
+          </article>
+          <article className="bg-secondary p-8 w-full max-w-[330px]  flex flex-col gap-5 items-center rounded-3xl">
+            <Image
+              src="/images/steps/earning.svg"
+              alt="art category"
+              // layout="fill"
+              width="250"
+              height="250"
+              objectFit="cover"
+            ></Image>
+
+            <div className="text-center">
+              <h3 className="text-2xl  font-bold mb-2">Create Collection</h3>
+              <p className="text-base">
+              Upload your work and setup your collection. Add a description, social links and floor price.
+              </p>
+            </div>
+          </article>
+          <article className="bg-secondary p-8 w-full max-w-[330px]  flex flex-col gap-5 items-center rounded-3xl">
+            <Image
+              src="/images/steps/wallet.svg"
+              alt="art category"
+              // layout="fill"
+              width="250"
+              height="250"
+              objectFit="cover"
+            ></Image>
+
+            <div className="text-center">
+              <h3 className="text-2xl  font-bold mb-2">Start Earning</h3>
+              <p className="text-base">
+              Choose between auctions and fixed-price listings. Start earning by selling your NFTs or trading others.
+              </p>
+            </div>
+          </article>
+        </div>
       </section>
     </main>
   );
